8c5e4f25f32de7379423b4059bc1faf13d5455f1
Chidera Paul Ugwuanyi <dexiouz@gmail.com>
2020-05-18 20:25:53 +0100

    dateTime picker

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import DateTimePicker from './src/components/DateTimePicker';
+const App = () => {
+  return <DateTimePicker />;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
